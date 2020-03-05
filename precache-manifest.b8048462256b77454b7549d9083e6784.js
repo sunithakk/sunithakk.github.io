c922eee3802426025d3b3387c003c0aa311b2303
sunitha hegde <sunitha@creatise.in>
2020-03-05 18:38:08 +0530

    init commit

---
+++ precache-manifest.b8048462256b77454b7549d9083e6784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dffd24f697ffe864d3b0d64922926089",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74ebd3290baead3418e9",
+    "url": "/static/css/2.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "9bdf4ae01d11babbef37",
+    "url": "/static/css/main.60cf38b0.chunk.css"
+  },
+  {
+    "revision": "74ebd3290baead3418e9",
+    "url": "/static/js/2.648358ca.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.648358ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bdf4ae01d11babbef37",
+    "url": "/static/js/main.34d11689.chunk.js"
+  },
+  {
+    "revision": "1fc8bfaec12784883e41",
+    "url": "/static/js/runtime-main.bf1927d7.js"
+  }
+]);
